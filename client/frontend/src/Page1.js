cc1092400ab3871797697d9dc2727917f09812b5
Ariel Elazarov <ariel.elazarov@yahoo.com>
2020-12-14 16:41:47 -0500

    ariel

--- client/frontend/src/Page1.js
+++ client/frontend/src/Page1.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReviewPage from '../src/Components/reviewpage';
+import {CardDeck} from 'react-bootstrap';
+import {Card } from 'react-bootstrap';
 
 const Page1 = () => {
     return (
@@ -14,6 +16,55 @@ const Page1 = () => {
                     <p className="lead">This is a modified jumbotron that occupies.</p>
                 </div>
             </div>
+
+            <h3 className="text-center sections">PODCASTS</h3>
+           
+            <CardDeck className="podcasts">
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      <h6 className="text-center">Watch</h6>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to additional
+                        content.{' '}
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <h6 className="text-center">Watch</h6>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to
+                        show that equal height action.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <h6 className="text-center">Watch</h6>
+                    </Card.Footer>
+                </Card>
+            </CardDeck>
+
+
+            <h3 className="text-center sections">BLOG POSTS</h3>
+
             <ReviewPage/>
 
         </div>

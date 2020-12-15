9c4a03647274ed306d680cc923a21ab8135acc4e
Steven Sarria <ssarr012@fiu.edu>
2020-12-15 16:10:34 -0500

    user login/signup + text fixes

---
+++ client/frontend/src/VideoPage.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactPlayer from "react-player"
+
+
+function VideoPage() {
+    return (
+        <div>
+            <ReactPlayer
+                url="https://www.youtube.com/watch?v=AJMxw_FsGZo&t=8s"
+            />
+        </div>
+    )
+}
+
+export default VideoPage

fdf20db0850e6956b1bee40afac45168433d8d66
Steven Sarria <ssarr012@fiu.edu>
2020-12-17 10:08:57 -0500

    added collections section

--- client/frontend/src/ReviewsPage.js
+++ client/frontend/src/ReviewsPage.js
@@ -10,6 +10,14 @@ const ReviewsPage = () => {
                     <h1 className="pt-3">Backlot Reviews</h1>
                 </div>
                 <ReviewPage />
+
+                <div className="container text-center pt-5">
+                    <button className="divider-button bg-dark mt-4" disabled></button>
+                    <h1 className="pt-3">Collections</h1>
+                    <br />
+                    <h3 className='mt-5'>Coming Soon</h3>
+                </div>
+
             </div>
         </div>
 
